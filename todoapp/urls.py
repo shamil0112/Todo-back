de82edf53ec3b92f52859dee03de93bea6ec8945
shamil106 <shamilmpd33@gmail.com>
2024-09-27 16:53:45 +0530

    first commit

---
+++ todoapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .import views
+
+urlpatterns = [
+    path("todos", views.todo_list),
+    path("todos/<int:pk>",views.todo_delail)
+]
+
+#urls:
+    # http://127.0.0.1:8000/todos
+    # http://127.0.0.1:8000/todos/id
